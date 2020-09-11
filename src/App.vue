3a547fd350d77eea904c1e763008ff322694f141
LuckShiba <luckshiba@protonmail.com>
2020-09-11 15:45:28 -0300

    navbar e fontawesome kk me perdoa

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,90 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <header class="navbar">
+      <div class="logo">
+        <!--logo aqui-->
+      </div>
+      <div class="nav-buttons">
+        <div class="nav-button">Início</div>
+        <div class="nav-button">Bots</div>
+        <div class="nav-button">Docs</div>
+        <div class="nav-button">Servidor</div>
+        <div class="nav-button">Equipe</div>
+      </div>
+      <div class="nav-button login-btn">Login <i class="fas fa-sign-in-alt"></i></div>
+    </header>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  margin: 0;
+  height: 100%;
+  background: var(--primary-background);
 }
 
-#nav {
-  padding: 30px;
+body.preload {
+  transition: none !important;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+*, input, button {
+  font-family: 'Roboto', sans-serif;
+  box-sizing: border-box;
+  border: none;
+  outline: none;
+  background: none;
+  margin: 0;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.navbar {
+  z-index: 3;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: var(--navbar-height);
+  background: var(--navbar-color);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  /*border-bottom: 1px solid var(--colors-border);*/
+  box-shadow: var(--shadow);
+}
+
+.navbar .logo {
+  fill: white;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+
+.logo svg {
+  max-width: 59px;
+  max-height: var(--navbar-height);
+  width: 100%;
+  height: 100%;
+}
+
+.nav-buttons {
+  display: flex;
+}
+
+.nav-buttons .nav-button {
+  margin: 0 5px;
+}
+
+.nav-button {
+  color: var(--nav-buttons-color);
+  padding: 10px;
+  transition: color .2s;
+}
+
+.nav-button:hover, .nav-button.active {
+  color: var(--normal-color);
+  cursor: pointer;
+}
+
+.login-btn i {
+  margin-left: 5px;
+  color: var(--icon-color);
+  transform: translateY(.5px);
 }
 </style>
