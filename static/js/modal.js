bf50cf4eaed6d83ff0b1d0bde06b63373251655d
LuckShiba <luckshiba@protonmail.com>
2020-09-10 22:20:00 -0300

    Deletei o BPD

--- static/js/modal.js
+++
@@ -1,4 +0,0 @@
-function showModal(id) {
-    $("#frame").attr("src", "/bots/" + id + "?frame=sim");
-    $("#botmodal").addClass("is-clipped").addClass("is-active");
-}
--- static/js/vote.js
+++
@@ -1,3 +0,0 @@
-function onSubmit(token) {
-    $("#votef").submit();
-}
